45067af38698cb0e5a0881dbf90eff6593a94c6e
AlawaththaKA <167974371+AlawaththaKA@users.noreply.github.com>
2025-04-28 01:36:36 +0530

    create skillpost back end

--- src/main/java/com/skillsharing/platform/skillsharing/SkillsharingApplication.java
+++ src/main/java/com/skillsharing/platform/skillsharing/SkillsharingApplication.java
@@ -12,27 +12,25 @@ import java.io.IOException;
 @SpringBootApplication
 public class SkillsharingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkillsharingApplication.class, args);
-	}
-
-
-
-@PostConstruct
-public void initFirebase() {
-    try {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/skillsharing-7659a-firebase-adminsdk-fbsvc-e3d351d411.json");
-
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-            System.out.println("✅ Firebase Initialized Successfully!");
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsharingApplication.class, args);
     }
-}
+
+    @PostConstruct
+    public void initFirebase() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/skillsharing-7659a-firebase-adminsdk-fbsvc-e3d351d411.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("✅ Firebase Initialized Successfully!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
